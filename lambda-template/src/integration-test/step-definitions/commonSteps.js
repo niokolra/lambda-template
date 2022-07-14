0e6862aeedfcbef55341f71c190a7607e2ea97bf
Shmuel Uri <shmuel.uri@nice.com>
2022-07-14 10:53:09 +0300

    add sam to IT

--- lambda-template/src/integration-test/step-definitions/commonSteps.js
+++ lambda-template/src/integration-test/step-definitions/commonSteps.js
@@ -1,13 +1,34 @@
 'use strict';
 const {When, Given} = require('@cucumber/cucumber');
 const {setDefaultTimeout} = require('@cucumber/cucumber');
+const child_process = require('node:child_process');
 setDefaultTimeout(60 * 1000);
 
-Given('event created',  () => {
+Given('event created', () => {
     console.log('event created');
 });
 
-When('local lambda invoked', () => {
+When('local lambda invoked', async () => {
     console.log('local lambda invoked');
-    //TODO: insert SAM local invoke
+    await run_script('sam local invoke');
+    console.log("Process Finished.");
 });
+
+function run_script(command) {
+
+    return new Promise((resolve, reject) => {
+        console.log("Starting Process.");
+        child_process.exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                reject(error);
+            }
+            if (stderr) {
+                console.log( ${stderr});
+            }
+            resolve(stdout);
+        });
+    });
+
+
+}
